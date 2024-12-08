92fe0c2c8e84ad48493e71ebec5439e2db863b75
kislate <2979574787@qq.com>
2024-12-08 17:16:38 +0800

    华为月赛，我是废物

---
+++ myFunction/myString.h
@@ -0,0 +1,23 @@
+#ifndef MYSTRING_H
+#define MYSTRING_H
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct MyString MyString;
+
+MyString* MyString_create(const char* str);
+void MyString_destroy(MyString* myString);
+const char* MyString_getString(const MyString* myString);
+void MyString_setString(MyString* myString, const char* str);
+char* MyString_toUpperCase(const MyString* myString);
+char* MyString_toLowerCase(const MyString* myString);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MYSTRING_H
