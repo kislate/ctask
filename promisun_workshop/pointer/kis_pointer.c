a08acb301dd45787cebde32f5318f932387902eb
kislate <2979574787@qq.com>
2024-12-16 18:23:12 +0800

    改了个垃圾问题

---
+++ promisun_workshop/pointer/kis_pointer.c
@@ -0,0 +1,27 @@
+#include <stdio.h> 
+#include <stdlib.h>
+char*strcopy(char*, const char *); 
+int main(void) 
+{
+    char *s1,*s2,*s3; 
+    s1 = (char*)malloc(50); //若分配空间，则指针指向未知，很危险
+    s2 = (char*)malloc(50); 
+    s3 = (char*)malloc(50); 
+    scanf("%s",s2); 
+    strcopy(s1,s2); 
+    scanf("%s",s2);
+    printf("%s\n",s1); 
+    s3=strcopy(s1,s2); 
+    printf("%s",s3);
+    free(s1); //释放
+    free(s2);
+    return 0; 
+}
+/*将字符串s复制给字符串t，并且返回串t的首地址*/ 
+char *strcopy (char *t,const char *s) 
+{
+	char *p=t; 
+    while(*t++ = *s++); 
+    return p; 
+}
+//为何头歌不过，无语。。。。。。。。。。
