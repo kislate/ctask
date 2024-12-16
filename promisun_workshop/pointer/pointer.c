45742ce09f1c91eefc8099d4b1e4cc0f7dd76d9e
Promisun-abc <161447118@qq.com>
2024-12-16 13:12:45 +0800

    为何头歌不过

--- promisun_workshop/pointer/pointer.c
+++ promisun_workshop/pointer/pointer.c
@@ -1,19 +1,27 @@
-#include<stdio.h> 
+#include <stdio.h> 
+#include <stdlib.h>
 char*strcopy(char*, const char *); 
 int main(void) 
 {
     char *s1,*s2,*s3; 
+    s1 = (char*)malloc(50); //若分配空间，则指针指向未知，很危险
+    s2 = (char*)malloc(50); 
+    s3 = (char*)malloc(50); 
     scanf("%s",s2); 
     strcopy(s1,s2); 
-    printf("%s\n",s1); 
     scanf("%s",s2);
+    printf("%s\n",s1); 
     s3=strcopy(s1,s2); 
-    printf("%s\n",s3);
+    printf("%s",s3);
+    free(s1); //释放
+    free(s2);
+	free(s3);
     return 0; 
 }
-/*灏嗗瓧绗︿覆s澶嶅埗缁欏瓧绗︿覆t锛屽苟涓旇繑鍥炰覆t鐨勯鍦板潃*/ 
+/*将字符串s复制给字符串t，并且返回串t的首地址*/ 
 char *strcopy (char *t,const char *s) 
 {
-    while(*t++=*s++); 
-    return (t); 
+	char *p=t; 
+    while(*t++ = *s++); 
+    return p; 
 }
