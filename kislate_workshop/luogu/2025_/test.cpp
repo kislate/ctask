b5ae8a39e5d86d0f3e1a3e4abd693fcb55129bec
kislate <2979574787@qq.com>
2025-03-14 22:02:24 +0800

    默认提交

--- kislate_workshop/luogu/2025_/test.cpp
+++ kislate_workshop/luogu/2025_/test.cpp
@@ -1,61 +1,29 @@
-// Creator: kislate
 #include <iostream>
 #include <vector>
-#include <utility>
-
 using namespace std;
+
+typedef struct{
+    int x, y;
+    int width, height;
+}carpet;
 
 int main(void)
 {
-   int MAX_MONEY, SUM_GOODS;
-   cin >> MAX_MONEY >> SUM_GOODS;
-   vector<vector<pair<int, int>>> all_goods(SUM_GOODS + 1, vector<pair<int, int>>(3, make_pair(0, 0)));
-   for(int i = 1; i <= SUM_GOODS; i++)
-   {
-        int price, value, type;
-        cin >> price >> value >> type;
-        if(type == 0)
-        {
-            all_goods[i][0].first = price;
-            all_goods[i][0].second = value*price;
-        }
-        else
-        {
-            if(all_goods[type][1].first == 0)
-            {
-                all_goods[type][1].first = price;
-                all_goods[type][1].second = value*price;
-            }
-            else
-            {
-                all_goods[type][2].first = price;
-                all_goods[type][2].second = value*price;
-            }
-        }
-   }
-   vector<int> dp_value(MAX_MONEY + 1, 0);
-    for(int i = 1; i <= SUM_GOODS; i++)
+    int n;
+    cin >> n;
+    vector<carpet>carpets(n);
+    for(int i = 0; i < n; i++)
     {
-        for(int j = MAX_MONEY; j >= 0; j--)
-        {
-            if(j >= all_goods[i][0].first)
-            {
-                dp_value[j] = max(dp_value[j], dp_value[j - all_goods[i][0].first] + all_goods[i][0].second);
-            }
-            if(j >= all_goods[i][0].first + all_goods[i][1].first)
-            {
-                dp_value[j] = max(dp_value[j], dp_value[j - all_goods[i][0].first - all_goods[i][1].first] + all_goods[i][0].second + all_goods[i][1].second);
-            }
-            if(j >= all_goods[i][0].first + all_goods[i][2].first)
-            {
-                dp_value[j] = max(dp_value[j], dp_value[j - all_goods[i][0].first - all_goods[i][2].first] + all_goods[i][0].second + all_goods[i][2].second);
-            }
-            if(j >= all_goods[i][0].first + all_goods[i][1].first + all_goods[i][2].first)
-            {
-                dp_value[j] = max(dp_value[j], dp_value[j - all_goods[i][0].first - all_goods[i][1].first - all_goods[i][2].first] + all_goods[i][0].second + all_goods[i][1].second + all_goods[i][2].second);
-            }
-            
-        } 
+        cin >> carpets[i].x >> carpets[i].y >> carpets[i].width >> carpets[i].height; 
     }
-    cout << dp_value[MAX_MONEY] << endl;
+    int nx, ny;
+    cin >> nx >> ny;
+    int ans = -1;
+    for(int i = 0; i < n; i++)
+    {
+        if(nx >= carpets[i].x && nx <=carpets[i].x + carpets[i].width && ny >=carpets[i].y && ny<=carpets[i].height+carpets[i].y)
+            ans = i + 1;
+    }
+    cout << ans << endl;
+    return 0;
 }
