7def094253566bc06e66129025ad0ba1c8bfef71
kislate <2979574787@qq.com>
2025-03-20 12:21:32 +0800

    尝试使用gitignore.

--- kislate_workshop/Problems/luogu/2025_spring/test_2.cpp
+++ kislate_workshop/Problems/luogu/2025_spring/test_2.cpp
@@ -1,46 +1,25 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-
-typedef struct{
-    int cost, gain, need, max_need_forward, max_need_backward, sum_cost, sum_gain;
-}node;
-
+// 不用long long会爆int
 int main(void)
 {
-    int n;
-    cin >> n;
-    vector<int> cost(n + 1);
-    vector<int> gain(n);
-
-    for (int i = 0; i < n + 1; ++i) cin >> cost[i];
-    for (int i = 0; i < n; ++i) cin >> gain[i];
-
-    vector<int> result(n);
-
-    for (int i = 0; i < n; ++i) {
-        int min_energy = cost[0];
-        int current_energy = cost[0];
-
-        for (int j = 0; j < n; ++j) {
-            if (j == i) {
-                current_energy -= cost[j+1];
-            } else {
-                current_energy += gain[j] - cost[j+1];
-            }
-
-            min_energy = min(min_energy, current_energy);
+    long long n, x;
+    cin >> n >> x;
+    vector<long long> loses(n);
+    vector<long long> wins(n);
+    vector<long long> uses(n);
+    vector<long long> dp(x+1);
+    for(long long i = 0; i < n; i++) cin >> loses[i] >> wins[i] >> uses[i];
+    for(long long i = 0; i < n; i++)
+    {
+        for(long long j = x; j >= 0; j--)
+        {
+            if(j < uses[i])
+                dp[j] += loses[i];
+            else
+                dp[j] = max(dp[j] + loses[i] ,dp[j-uses[i]] + wins[i]);
         }
-        min_energy -= cost[n];
-
-        result[i] = max(0, -min_energy);
     }
-
-    for (int i = 0; i < n; ++i) {
-        cout << result[i] << (i == n - 1 ? "" : " ");
-    }
-    cout << endl;
-
+    cout << dp[x]*5 << endl;
     return 0;
 }
