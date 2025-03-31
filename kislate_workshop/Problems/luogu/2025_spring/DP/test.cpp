c8aafd59f9af5130e6d321dfd3be0371e70c88d6
kislate <2979574787@qq.com>
2025-03-31 21:28:41 +0800

    伤心了

--- kislate_workshop/Problems/luogu/2025_spring/DP/test.cpp
+++ kislate_workshop/Problems/luogu/2025_spring/DP/test.cpp
@@ -1,26 +1,91 @@
 #include <bits/stdc++.h>
 using namespace std;
-int max_five(int a, int b, int c, int d, int e)
-{
-    return max(a, max(b, max(c, max(d, e))));
-}
-int main(void)
-{
-    int row, col, max_length = 1;
-    cin >> row >> col;
-    vector<vector<int>> map_height(row+2, vector<int>(col+2, 0)), dp(row+2, vector<int>(col+2, 1));
-    for(int i = 1; i < row; i++) for(int j = 0; j < col; j++) cin >> dp[i][j];
-    for(int i = 1; i <= row; i++)
-    {
-        for(int j = 1; j <= col; j++)
+
+typedef struct landmine{
+   int  num;
+   int  sum;
+   int  prior;
+}landmine;
+
+int main(void){
+    int n;
+    cin >> n;
+    vector<vector<int>> nodeptr(n + 3, vector<int>(n + 100, 0));// 打算把第一位当作储存连接的点的个数
+    vector<landmine> node(n + 3, {0, 0, 0});
+    for(int i = 1; i <= n; i++) cin >> node[i].num, node[i].prior = 0;
+    for(int i = 1; i <= n-1; i++){
+        int cnt = 0;
+        for(int j = 1; j <= n-i; j++)
         {
-            if(map_height[i+1][j] > map_height[i][j])    dp[i+1][j] = max(dp[i][j] + 1, dp[i+1][j]);
-            if(map_height[i-1][j] > map_height[i][j])    dp[i-1][j] = max(dp[i][j] + 1, dp[i-1][j]);
-            if(map_height[i][j+1] > map_height[i][j])    dp[i][j+1] = max(dp[i][j] + 1, dp[i][j+1]);
-            if(map_height[i][j-1] > map_height[i][j])    dp[i-1][j-1] = max(dp[i][j] + 1, dp[i][j-1]);
-            max_length = max_five(max_length, dp[i+1][j], dp[i-1][j], dp[i][j+1], dp[i][j-1]);
+            int type;
+            cin >> type;
+            if(type) nodeptr[i][++cnt] = i + j;
+        }
+        nodeptr[i][0] = cnt;
+        cnt = 0;
+    }
+    // // 调试:
+    // for(int i = 1; i <= n; i++){
+    //     cout << node[i].num << " ";
+    // }
+    // cout << endl;
+    // for(int i = 1; i <= n-1; i++)
+    // {
+    //     for(int j = 1; j <= nodeptr[i][0]; j++)
+    //     {
+    //         cout << nodeptr[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    pair<int, int> ans_sum_node = {0, 0};
+
+    vector<int> mem(n + 3, 0);
+    mem[1] = ans_sum_node.first;
+    int node_num = ans_sum_node.first, cnt = 1, cnt_final = 0, start = 0;
+
+    for(int m = 1; m <= n; m++)
+    {
+        bool change = false;
+        //node[1].sum = node[1].num;
+        node[m].sum = node[m].num;
+        if(node[m].sum > ans_sum_node.second){
+            ans_sum_node.first = m;
+            ans_sum_node.second = node[m].sum;
+            change = true;
+            start = m;
+        }
+        for(int i = m; i <= n-1; i++){
+            for(int j = 1; j <= nodeptr[i][0]; j++){
+                if(node[nodeptr[i][j]].sum < node[i].sum + node[nodeptr[i][j]].num){
+                    node[nodeptr[i][j]].sum = node[i].sum + node[nodeptr[i][j]].num;
+                    node[nodeptr[i][j]].prior = i;
+                }
+                if(node[nodeptr[i][j]].sum > ans_sum_node.second){
+                    ans_sum_node.first = nodeptr[i][j];
+                    ans_sum_node.second = node[nodeptr[i][j]].sum;
+                    change = true;
+                }
+            }
+        }
+
+        if(change){
+            node_num = ans_sum_node.first;
+            while(node_num != m){
+                node_num = mem[++cnt] = node[node_num].prior;
+            }
+            mem[m] = ans_sum_node.first;
+            cnt_final = cnt;
+            start = m;
+        }
+        // 清空:
+        for(int i = 1; i <= n; i++){
+            node[i].sum = 0;
+            node[i].prior = 0;
         }
     }
-    cout << max_length << endl;
-    return 0;
+    // 输出顺序:
+    for(int i = cnt_final; i >= start; i--){
+        cout << mem[i] << " ";
+    }
+    cout << endl << ans_sum_node.second << endl;
 }
