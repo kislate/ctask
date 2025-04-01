3b3bb517f04a156f25a96c5e85b1c2a81581d385
kislate <2979574787@qq.com>
2025-04-01 11:26:34 +0800

    更新两份ac代码

--- kislate_workshop/Problems/luogu/2025_spring/DP/test.cpp
+++ kislate_workshop/Problems/luogu/2025_spring/DP/test.cpp
@@ -1,91 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-typedef struct landmine{
-   int  num;
-   int  sum;
-   int  prior;
-}landmine;
-
-int main(void){
-    int n;
-    cin >> n;
-    vector<vector<int>> nodeptr(n + 3, vector<int>(n + 100, 0));// 打算把第一位当作储存连接的点的个数
-    vector<landmine> node(n + 3, {0, 0, 0});
-    for(int i = 1; i <= n; i++) cin >> node[i].num, node[i].prior = 0;
-    for(int i = 1; i <= n-1; i++){
-        int cnt = 0;
-        for(int j = 1; j <= n-i; j++)
-        {
-            int type;
-            cin >> type;
-            if(type) nodeptr[i][++cnt] = i + j;
-        }
-        nodeptr[i][0] = cnt;
-        cnt = 0;
-    }
-    // // 调试:
-    // for(int i = 1; i <= n; i++){
-    //     cout << node[i].num << " ";
-    // }
-    // cout << endl;
-    // for(int i = 1; i <= n-1; i++)
-    // {
-    //     for(int j = 1; j <= nodeptr[i][0]; j++)
-    //     {
-    //         cout << nodeptr[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
-    pair<int, int> ans_sum_node = {0, 0};
-
-    vector<int> mem(n + 3, 0);
-    mem[1] = ans_sum_node.first;
-    int node_num = ans_sum_node.first, cnt = 1, cnt_final = 0, start = 0;
-
-    for(int m = 1; m <= n; m++)
+void print(int i, vector<int> prior){
+    if(prior[i] == 0) 
     {
-        bool change = false;
-        //node[1].sum = node[1].num;
-        node[m].sum = node[m].num;
-        if(node[m].sum > ans_sum_node.second){
-            ans_sum_node.first = m;
-            ans_sum_node.second = node[m].sum;
-            change = true;
-            start = m;
-        }
-        for(int i = m; i <= n-1; i++){
-            for(int j = 1; j <= nodeptr[i][0]; j++){
-                if(node[nodeptr[i][j]].sum < node[i].sum + node[nodeptr[i][j]].num){
-                    node[nodeptr[i][j]].sum = node[i].sum + node[nodeptr[i][j]].num;
-                    node[nodeptr[i][j]].prior = i;
-                }
-                if(node[nodeptr[i][j]].sum > ans_sum_node.second){
-                    ans_sum_node.first = nodeptr[i][j];
-                    ans_sum_node.second = node[nodeptr[i][j]].sum;
-                    change = true;
-                }
-            }
-        }
-
-        if(change){
-            node_num = ans_sum_node.first;
-            while(node_num != m){
-                node_num = mem[++cnt] = node[node_num].prior;
-            }
-            mem[m] = ans_sum_node.first;
-            cnt_final = cnt;
-            start = m;
-        }
-        // 清空:
-        for(int i = 1; i <= n; i++){
-            node[i].sum = 0;
-            node[i].prior = 0;
-        }
+        printf("%d", i);
+        return;
     }
-    // 输出顺序:
-    for(int i = cnt_final; i >= start; i--){
-        cout << mem[i] << " ";
+    else
+    {
+        print(prior[i], prior);
+        printf(" %d", i);
     }
-    cout << endl << ans_sum_node.second << endl;
+}
+int main(void){
+    int N;
+    cin >> N;
+    vector<int> prior(N + 1, 0), dp(N + 1, 0), num(N + 1, 0);
+    vector<vector<int>> path(N + 1, vector<int>(N + 1, 0));
+    for(int i = 1; i <= N; i++) cin >> num[i];
+    for(int i = 1; i <= N-1; i++) for(int j = i + 1; j <= N; j++) cin >> path[i][j];
+    int ans = num[1], ans_index = 1;
+    for(int i = 1; i <= N; i++)
+    {
+        for(int j = 1; j <= N; j++)
+        {
+            if(path[j][i] && dp[i] < dp[j]) dp[i] = dp[j], prior[i] = j;
+        }
+        dp[i] += num[i];
+        if(dp[i] > ans) ans = dp[i], ans_index = i;
+    }
+    print(ans_index, prior);
+    cout << endl;
+    cout << ans << endl;
 }
