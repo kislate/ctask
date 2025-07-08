3473c409548fff151200a2fd25de68775d7a5719
kislate <2979574787@qq.com>
2025-07-08 15:56:26 +0800

    例行更新

--- kislate_workshop/Problems/luogu/2025_spring/string_luogu/test_string.cpp
+++ kislate_workshop/Problems/luogu/2025_spring/string_luogu/test_string.cpp
@@ -1,27 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+unordered_set<string> myset;
 vector<bool> dp;
-vector<string> myset;
-int sum = 0;
 int main(void)
 {
-	string temp;
-	while(cin >> temp)
-	{
-		myset.push_back(temp}
-		sum++;
-	}
-	string S;
-	cin >> S;
-	len = S.length();
-	for(int i = 0; i < len; i++)
-	{
-		for(int j = 0; j < i; j++)
-		{
-			if(dp[j])
-			{
-				
-			}
-		}
-	} 
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string temp;
+    while(cin >> temp)
+    {
+        if(temp == ".") break;
+        myset.insert(temp);
+    }
+    cin.ignore();  // 清理缓冲区中的换行符
+    string s, line;
+    for(string temp;cin>>temp;s+=temp);
+    int len = (int)s.length();
+    dp.resize(len + 1, false);
+    dp[0] = true;
+    int ans = 0;
+    for(int i = 1; i <= len; i++)  // 从1开始
+    {
+        for(int j = 0; j < i; j++)
+        {
+            if(dp[j] && myset.count(s.substr(j, i - j)))
+            {
+                dp[i] = true;
+                ans = max(ans, i);
+                break;
+            }
+        }
+    }
+    cout << ans << endl;
+    return 0;
 }
